4b2e32be8fe5eeef7ca7a21378480d01fe69ba19
Bao <baoquoczero@gmail.com>
2024-05-06 15:23:26 +0700

    Test Code for Jenkins Test Code for Jenkins

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("Xin chaoo");
+    System.out.println("Xin chao");
     GradeConverter converter = new GradeConverter();
 
     for (int i = 0; i < 105; i++) {
